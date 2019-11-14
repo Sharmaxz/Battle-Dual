abbbbb0b4a7710891dceb32df98eaad9c945710e
Shadow <shadowvonwolfkin@gmail.com>
2019-11-14 01:42:58 -0200

    Created the activity initial

--- app/src/main/java/com/infnet/battle_dual/MainActivity.kt
+++ app/src/main/java/com/infnet/battle_dual/MainActivity.kt
@@ -2,11 +2,25 @@ package com.infnet.battle_dual
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.util.DisplayMetrics
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        val metrics = resources.displayMetrics
+
+        //Remove notification bar
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_initial)
     }
 }
